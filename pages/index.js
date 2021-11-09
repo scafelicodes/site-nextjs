0e98ebf754cf0138d3a585153bf24df985d97b51
Gustavo <g.cflsouza@gmail.com>
2021-11-09 09:16:35 -0300

    reconstruindo o site do zero

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
-
 import Header from "./../components/header";
 import Footer from "./../components/footer";
 import Hero from "./../components/hero";
