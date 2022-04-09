463647bd9156fa9af932dc94040e661d0aaf6d8f
Gustavo <g.cflsouza@gmail.com>
2022-04-08 23:25:43 -0300

    menu header

--- components/footer/index.js
+++ components/footer/index.js
@@ -5,6 +5,7 @@ export default function Footer() {
     <footer>
       <div className="container text-center">
         <h3>Vamos criar um projeto juntos?</h3>
+        <p>Eu posso ajudar o seu projeto desde a engenharia ao lançamento</p>
         <Link href="/contato">
           <a className="main-link">
             Entrar em contato <i className="gg-arrow-right"></i>
--- components/header/index.js
+++ components/header/index.js
@@ -1,22 +1,46 @@
 import Link from "next/link";
 
+import { elastic as Menu } from "react-burger-menu";
+
 export default function Header() {
   return (
-    <div className="header">
-      <div className="container">
-        <Link href="/">
-          <a className="logo-header">
-            <img
-              src="https://images.unsplash.com/profile-1648163079054-73b1ad95065fimage?dpr=1&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff"
-              alt="Logo"
-            />
-            <div>
-              <h3>Gustavo Scafeli</h3>
-              <p>Software Engineer</p>
-            </div>
-          </a>
-        </Link>
+    <>
+      <div className="header">
+        <div className="container">
+          <Link href="/">
+            <a className="logo-header">
+              <img
+                src="https://images.unsplash.com/profile-1648163079054-73b1ad95065fimage?dpr=1&auto=format&fit=crop&w=150&h=150&q=60&crop=faces&bg=fff"
+                alt="Logo"
+              />
+              <div>
+                <h3>Gustavo Scafeli</h3>
+                <p>Software Engineer</p>
+              </div>
+            </a>
+          </Link>
+
+          <Menu right>
+            <Link href="/">
+              <a id="home" className="menu-item">
+                Home <i className="gg-arrow-right"></i>
+              </a>
+            </Link>
+
+            <Link href="/sobre">
+              <a id="about" className="menu-item">
+                Sobre <i className="gg-arrow-right"></i>
+              </a>
+            </Link>
+
+            <Link href="/contato">
+              <a id="contact" className="menu-item">
+                Contato <i className="gg-arrow-right"></i>
+              </a>
+            </Link>
+          </Menu>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
