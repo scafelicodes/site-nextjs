dc8261424dec515b91caf9e611325f74662be518
Gustavo <g.cflsouza@gmail.com>
2022-05-12 21:59:07 -0300

    update

--- pages/links.js
+++ pages/links.js
@@ -5,25 +5,31 @@ export default function Links() {
     <div className="bio-page-scafeli">
       <div className="container bio">
         <div className="text-center">
-          <img className="logo-header" src="logo-scafeli-white.svg" />
-          <p className="text-bio">
-            Fácil de entender, impossível de ignorar ⚡
-          </p>
+          <img className="logo-header" src="scafeli-dark.svg" height={30} />
+          <h1 className="">
+            Scafeli
+            {/* <img src="verified.svg" height={30} /> */}
+          </h1>
+          <p className="text-bio">Fácil de entender, impossível de ignorar.</p>
         </div>
 
         <ul>
           <li>
-            <Link href="/contato">
-              <a>
-                <i class="fab fa-whatsapp"></i> Solicite uma proposta
-              </a>
-            </Link>
+            <a href="#" target="_blank">
+              <i class="fab fa-whatsapp"></i> Solicite uma proposta 📲
+            </a>
           </li>
 
           <li>
-            <Link href="/trabalhos">
+            <a href="#" target="_blank">
+              <i class="fas fa-mask"></i> Últimos trabalhos 🔥
+            </a>
+          </li>
+
+          <li>
+            <Link href="/">
               <a>
-                <i class="fas fa-mask"></i> Últimos trabalhos
+                <i class="fab fa-spotify"></i> Nosso site 💻
               </a>
             </Link>
           </li>
@@ -33,17 +39,19 @@ export default function Links() {
               target="_blank"
               href="https://open.spotify.com/playlist/1pAw0anb2Ntp0cR5DJFpWf?si=6f0e4777ab8d464c"
             >
-              <i class="fab fa-spotify"></i> Nossa playlist no Spotify
+              <i class="fab fa-spotify"></i> Estamos no Spotify 🎵
+            </a>
+          </li>
+
+          <li>
+            <a target="_blank" href="https://unsplash.com/@scafeli">
+              <i class="fab fa-spotify"></i> Material gratuito no Unsplash 📸
             </a>
           </li>
         </ul>
 
         <div className="footer-links">
-          <Link href="/">
-            <a>
-              <i className="gg-arrow-left"></i> Visite o nosso site
-            </a>
-          </Link>
+          <img className="logo-footer" src="scafeli-dark.svg" />
         </div>
       </div>
     </div>
